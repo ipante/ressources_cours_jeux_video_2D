826975a178af9099c67c555dfae7678c47dc8160
Isaac Pante <isaac.pante@unil.ch>
2019-08-23 20:14:36 +0200

    Update game.js

--- phaser_animation_sans_atlas/game.js
+++ phaser_animation_sans_atlas/game.js
@@ -5,8 +5,11 @@ const config = {
     height : 400,
     pixelArt: true,
     backgroundColor : 'black',
-    physics : {
-        default : 'arcade'
+    // définir la physique pour
+    // créer des collisions avec
+    // les limites du monde
+    physics: {
+        default: 'arcade',
     },
     scene : {
         preload : preload,
@@ -18,40 +21,56 @@ const config = {
 let jeu = new Phaser.Game(config);
 
 function preload(){
-    // chargement de la spritesheet (pas d'atlas ici)
+    // chargement de la spritesheet
+    // parce que les sprites ont une taille homogène
+    // et sont tous dans le même sens, on peut calculer
+    // directement les images, sans recourir à un atlas
+    // json (cf. autre exemple)
     this.load.spritesheet("heros","assets/warrior.png",{
+        // dimensions de chaque sprite
         frameWidth : 98,
         frameHeight : 101
     }) 
 }
 
 function create(){
-    this.heros = this.physics.add.sprite(100,100,'heros',0)
+    // ajout du sprite d'origine
+    this.heros = this.physics.add.sprite(100,100,'heros',12);
+    // ajout de la physique
+    this.heros.setBounce(0.2);
+    this.heros.setCollideWorldBounds(true);
+
     // création de l'animation de marche
     this.anims.create({
+        // nom de l'animation
         key : 'gauche',
-        repeat : 1,
-        framerate : 10,
+        // ne pas mettre de répétition,
+        // pour que l'animation s'arrête
+        // avec la touche
+        repeat : 0,
+        // vitesse de l'animation
+        frameRate : 15,
+        // retenir les frames nécessaires
         frames : this.anims.generateFrameNames('heros',{
-            start : 1, end : 4
+            start : 0, end : 3
             }
          )
     })
     this.anims.create({
         key : 'haut',
-        repeat : 1,
-        framerate : 10,
+        repeat : 0,
+        frameRate : 15,
         frames : this.anims.generateFrameNames('heros',{
-            start : 7, end : 10
+            start : 6, end : 9
             }
          )
     })
     this.anims.create({
         key : 'bas',
-        repeat : 1,
-        framerate : 10,
+        repeat : 0,
+        frameRate : 15,
         frames : this.anims.generateFrameNames('heros',{
-            start : 11, end : 14
+            start : 12, end : 15
             }
          )
     })      
@@ -61,26 +80,43 @@ function create(){
 
 }    
 
-function update(){
-
-    this.heros.setVelocityX(0);
-
+function update(){  
+    // définit la vitesse par défaut
+    this.heros.setVelocity(0,0);
     if (fleches.left.isDown)
     {
-        this.heros.play('gauche')
-        this.heros.setVelocityX(-300);
+        // ce code doit être ajouté pour rétablir
+        // le personnage dans la bonne direction
+        // après un déplacement vers la droite
+        if(this.heros.flipX){
+            this.heros.flipX = false;
+        }
+        this.heros.setVelocityX(-100);
+        this.heros.anims.play('gauche',true);
     }
     else if (fleches.right.isDown)
     {
-        this.heros.play('gauche')
+        // notre SpriteSheet n'a pas d'images
+        // pour la droite ; nous allons donc prendre
+        // l'animation pour "gauche" et la retourner
+        this.heros.setVelocityX(100);
+        this.heros.anims.play('gauche',true).flipX = true;
     }
-
-    if (fleches.up.isDown)
+    else if (fleches.up.isDown)
     {
-        this.heros.play('haut')
+        this.heros.setVelocityY(-100);
+        this.heros.anims.play('haut',true);
     }
     else if (fleches.down.isDown)
     {
-        this.heros.play('bas')
-    }    
+        this.heros.setVelocityY(100);
+        this.heros.anims.play('bas',true);
+    }
+    // ATTENTION! LE CODE QUI SUIT PRODUIT UNE
+    // ERREUR : préférer la ligne 88
+    // else
+    // {
+    //     this.heros.setVelocity(0,0);
+    // }
+    
 }
