dfdc057200f04876378beb17454530e81d5ed5ea
Isaac Pante <isaac.pante@unil.ch>
2021-04-20 09:22:25 +0200

    Mise à jour du Tilemap

--- phaser_tilemap/game.js
+++ phaser_tilemap/game.js
@@ -1,69 +1,38 @@
-let game;
-
-window.onload = function() {
-    let gameConfig = {
-        type: Phaser.AUTO,
-        width: 420,
-        height: 735,
-        parent: "game-container",
-        // scale : NONE,
-        // zoom : 1,
-        scene: playGame,
-        backgroundColor: 0x444444,
-        // scale: {
-        //     mode: Phaser.Scale.FIT,
-        //     autoCenter: Phaser.Scale.CENTER_BOTH,
-        // }
+let gameConfig = {
+    type: Phaser.AUTO,
+    parent: "game-container",
+    zoom : 0.8, // ajuster le zoom pour la netteté
+    scale: { // le scale manager pour le calibrage
+        parent: 'game-container',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width : 128, // soit 16*8
+        height : 224 // soit 16*14
+    },
+    scene : {
+        preload : preload,
+        create : create
     }
-    jeu = new Phaser.Game(gameConfig);
-    window.focus();
-    resize();
-    window.addEventListener("resize", resize, false);
 }
 
-// playGame scene
-class playGame extends Phaser.Scene{
-    constructor(){
-        super("PlayGame");
-    }
-    preload(){
-        // charger la spritesheet
-        this.load.image('image_tileset','assets/OldAdelaide.bmp');
-        // charger le fichier json associé
+let jeu = new Phaser.Game(gameConfig);
+
+function preload(){
+        // charger le tileset
+        this.load.image('image_tileset','assets/DesertSands.png');
+        // charger le fichier json associé (tilemap)
         this.load.tilemapTiledJSON('carte_json','assets/carte.json');
-    }
-    create(){
+}
+
+function create(){
         // charger la carte
         let carte = this.add.tilemap('carte_json');
         // lier le tileset
         // attention : le premier élément est le nom
         // du tileset défini dans "Tiled"
-        let terrain = carte.addTilesetImage('OldAdelaide','image_tileset');
-        //carte.setBaseTileSize(82,82);
-        // carte.getTilesWithinWorldXY(1334,750,30,30)
-        // créer les layers
+        let terrain = carte.addTilesetImage('DesertSands','image_tileset');
         // attention : les premiers paramètres sont les noms
         // des layers définis dans "Tiled"
-        let fond = carte.createStaticLayer('fond',[terrain],0,0).setDepth(-1).setScale(3.27);
-        let couche1 = carte.createStaticLayer('couche1',[terrain],0,0).setScale(3.27);
-    }
-    update(){
-
-    }
-};
-// fonction pour occuper tout l'écran
-function resize(){
-    let canvas = document.querySelector("canvas");
-    let windowWidth = window.innerWidth;
-    let windowHeight = window.innerHeight;
-    let windowRatio = windowWidth / windowHeight;
-    let gameRatio = jeu.config.width / jeu.config.height;
-    if(windowRatio < gameRatio){
-        canvas.style.width = windowWidth + "px";
-        canvas.style.height = (windowWidth / gameRatio) + "px";
-    }
-    else{
-        canvas.style.width = (windowHeight * gameRatio) + "px";
-        canvas.style.height = windowHeight + "px";
-    }
+        let fond = carte.createLayer('fond',[terrain],0,0).setDepth(-1);
+        let couche1 = carte.createLayer('objets',[terrain],0,0);
 }
