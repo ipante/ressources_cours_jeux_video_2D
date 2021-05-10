a38ca045c9de0858d06727bcc54854fc77074caf
Isaac Pante <isaac.pante@unil.ch>
2021-05-10 10:04:39 +0200

    ajout d'un frogger 3

---
+++ Python_JS.js
@@ -0,0 +1,31 @@
+/* PYTHON -> JS */
+
+// environnement (web & node)
+
+// commentaires
+
+// variables & scope
+
+// objets (== lists)
+
+// tableaux (== dict)
+
+// boucles & conditions
+
+// opérateurs
+
+// opérations sur tableaux (map, filter, reduce)
+
+// fonctions
+
+// fonctions fléchées (== lambda)
+
+// destructuring
+
+// paramètre du reste
+
+// callback
+
+// promesses
+
+// debug
